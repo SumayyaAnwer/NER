c32a578c244ca13e8dd9e05db1fca5ef208bb9f9
SumayyaAnwer <146512240+SumayyaAnwer@users.noreply.github.com>
2024-06-24 22:05:25 +0530

    Update ass.py

--- ass.py
+++ ass.py
@@ -1,28 +1,48 @@
+
+# library for streamlit
 import streamlit as st
-from newspaper import Article
-import spacy
+#importing libraries for NER
 from spacy import displacy
+import en_core_web_sm
 
-nlp = spacy.load("en_core_web_sm")
+nlp = en_core_web_sm.load()
 
-st.title("Named Entity Recognition")
+#library for extracting text from URL
+from newspaper import Article
+from pprint import pprint
 
+#function to extract NER
+def ner_analyser(text):
+    doc = nlp(text)
+    ent_html = displacy.render(doc, style="ent", jupyter=False)
+    st.markdown(ent_html, unsafe_allow_html=True)
+    return
+#UI code
+st.title("NER Demo")
+url = st.text_input("Enter URL")
+st.write("OR")
+text= st.text_area("Enter paragraph")
 
-input_url = st.text_area("Enter URL here:")
-input_paragraph = st.text_area("Enter paragraph here:")
+#button to analyze
+if(st.button("Analyze")):
+#checking if both URL and Text are entered
+    if text and url:
+        st.write("Please enter either URL or Text to analyze")
+#checking if only text is entered
+    elif text:
+        ner_analyser(text)
+#checking if only URL is entered
+    elif url :
+        #extracting text from URL
+        try:
+            article = Article(url)
+            article.download()
+            article.parse()
+            url_text = article.text
+            ner_analyser(url_text)
 
-if st.button("Analyze"):
-    if input_url:
-        article = Article(input_url)
-        article.download()
-        article.parse()
-        doc = nlp(article.text)
-        html = displacy.render(doc, style='ent', jupyter=False)
-        st.markdown(html, unsafe_allow_html=True)
-    elif input_paragraph:
-        doc = nlp(input_paragraph)
-        html = displacy.render(doc, style='ent', jupyter=False)
-        st.markdown(html, unsafe_allow_html=True)
+        except Exception as e:
+            st.write("Please enter valid URL")
+#checking if nothing is entered
     else:
-        st.write("Please enter a URL or a paragraph.")
-
+        st.write("Please enter URL or Text to analyze")
